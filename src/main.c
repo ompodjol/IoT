5ff89b2abeea475153e8c79aba4a3277416e01d6
Jolly Jae Ompod <jollyjae.ompod@tietoevry.com>
2024-12-13 15:52:02 +0100

    fix build error.

--- src/main.c
+++ src/main.c
@@ -1,8 +1,26 @@
 // main.c
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
+#include <cmocka.h>
 #include "hello.h"
+
+#int main(void)
+#{
+#print_hello();
+#return 0;
+#}
+
+// Declare your test function
+static void test_print_hello(void **state)
+{
+    // Your test code here
+}
 
 int main(void)
 {
-    print_hello();
-    return 0;
+    const struct CMUnitTest tests[] = {
+        cmocka_unit_test(test_print_hello),
+    };
+    return cmocka_run_group_tests(tests, NULL, NULL);
 }
